5a998c6738fca922763a37fd4abe54fcfcf2a346
coder2612 <56359029+coder2612@users.noreply.github.com>
2020-10-03 19:29:21 +0530

    Update main.js

--- Jungle-Safari/main.js
+++ Jungle-Safari/main.js
@@ -1,22 +1,18 @@
 const animals = {
   wolf: {
-    fact:
-      "Wolves are the largest members of the dog family. Wolves can roam large and long distances, sometimes up to 12 miles (20 kilometers) in a single day.",
+    fact: "Wolves are the largest members of the dog family. Wolves can roam large and long distances, sometimes up to 12 miles (20 kilometers) in a single day. One wolf can eat 20 pounds (9 kilograms) of meat at a single sitting.",
     music: "./sounds/wolf.mp3"
   },
   lion: {
-    fact:
-      "Lions are the second largest big cat species in the world (behind tigers).The average male lion weighs around 180 kg (400 lb) while the average female lion weighs around 130 kg (290 lb).",
+    fact: "Lions are the second largest big cat species in the world (behind tigers).The average male lion weighs around 180 kg (400 lb) while the average female lion weighs around 130 kg (290 lb). It is sexually dimorphic; adult male lions have a prominent mane.",
     music: "./sounds/lion.mp3"
   },
   elephant: {
-    fact:
-      "Elephants are the world’s largest land animal! Male African elephants can reach 3m tall and weigh between 4,000 -7,500kg. Asian elephants are slightly smaller, reaching 2.7m tall and weighing 3,000– 6,000kg.",
+    fact: "Elephants are the world’s largest land animal! Male African elephants can reach 3m tall and weigh between 4,000 -7,500kg. Asian elephants are slightly smaller, reaching 2.7m tall and weighing 3,000– 6,000kg. They communicate through vibrations.",
     music: "./sounds/elephant.mp3"
   },
   crocodile: {
-    fact:
-      "Crocodiles are large reptiles that live in fresh water, lakes, rivers, brackish water (mix between salty and fresh water). They can be found in tropic areas of Australia, Africa, America and Asia.",
+    fact: "Crocodiles are large reptiles that live in fresh water, lakes, rivers, brackish water (mix between salty and fresh water). They can be found in tropic areas of Australia, Africa, America and Asia.",
     music: "./sounds/crocodile.mp3"
   }
 };
